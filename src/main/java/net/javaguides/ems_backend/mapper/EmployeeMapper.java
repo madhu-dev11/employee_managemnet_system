98059ce241a19df382faaf685b4202d2f507b5c0
madhual <madhulokesh1117@gmail.com>
2026-04-21 14:41:19 +0530

    mapping

---
+++ src/main/java/net/javaguides/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package net.javaguides.ems_backend.mapper;
+
+import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstname(),
+                employee.getLastname(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstname(),
+                employeeDto.getLastname(),
+                employeeDto.getEmail()
+        );
+    }
+}

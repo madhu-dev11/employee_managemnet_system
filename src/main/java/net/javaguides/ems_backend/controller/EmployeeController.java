b720268e30f8d2ef96edaa0c48af958be284a387
madhual <madhulokesh1117@gmail.com>
2026-04-22 13:18:06 +0530

    Add Get All Employees REST APIS

--- src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -39,4 +39,19 @@ public class EmployeeController {
       List<EmployeeDto> employees =  employeeService.getAllEmployees();
       return ResponseEntity.ok(employees);
     }
+
+    //BUILD Update EMPLOYEES REST API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,
+                                                      @RequestBody EmployeeDto  updatedEmployee){
+      EmployeeDto employeeDto =  employeeService.updateEmployee(employeeId, updatedEmployee);
+        return  ResponseEntity.ok(employeeDto);
+    }
+    //BUILD delete EMPLOYEES REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully!...");
+    }
+
 }
